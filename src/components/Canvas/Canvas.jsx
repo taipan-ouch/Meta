0abb7af877c5b2bc1a0d9fd81477de83c3f5aca6
Tim Noonan <tim.noonan@me.com>
2020-05-06 21:03:13 -0600

    feat: high level ui components

---
+++ src/components/Canvas/Canvas.jsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+
+export default function Canvas() {
+  return (
+      <div id="root"></div>
+  );
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,15 @@
 import * as React from 'react';
 import { render } from 'react-dom';
 
-import App from './App';
+import App from 'App';
+import Sidebar from 'components/Sidebar/Sidebar'
+import Canvas from 'components/Canvas/Canvas'
+
+const sidebarContainer = document.getElementById('sidebar');
+render(<Sidebar />, sidebarContainer);
+
+const canvasContainer = document.getElementById('canvas');
+render(<Canvas />, canvasContainer);
 
 const rootElement = document.getElementById('root');
 render(<App />, rootElement);
